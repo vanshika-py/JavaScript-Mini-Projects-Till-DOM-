732be06d93e649fed06d299071b851a2374a4047
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-01-13 20:29:46 +0530

    model tasks first commit

---
+++ Event Delegation Tasks/script.js
@@ -0,0 +1,12 @@
+let Button = document.querySelector('.Btn');
+let modelcontainer = document.querySelector('.model-container');
+let closeButton = document.querySelector('.close-button');
+
+
+Button.addEventListener('click',()=> {
+  modelcontainer.style.display = 'flex';
+})
+
+closeButton.addEventListener('click', ()=>{
+modelcontainer.style.display = 'none'
+});
