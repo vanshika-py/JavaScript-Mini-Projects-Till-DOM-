32081c0ee0d6a3454137e6126f1f61303290bf83
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-01-17 23:20:58 +0530

    done small part

--- Counter App/script.js
+++ Counter App/script.js
@@ -1,1 +1,23 @@
-let incBtn = document.querySelector('.increaseBtn');
+let initialValue = document.querySelector('.initial-value');
+let increaseButton = document.querySelector('.increaseBtn');
+let decreaseButton = document.querySelector('.decreaseBtn');
+let resetButton = document.querySelector('.resetBtn'); 
+
+initialValue = 1;
+
+increaseButton.addEventListener('click', () => {
+    let currentValue = initialValue++;
+    console.log(`Clicked  ${currentValue} times`);
+});
+
+
+decreaseButton.addEventListener('click', () => {
+    let currentValue = initialValue++;
+    console.log(`Clicked  ${currentValue} times`);
+});
+
+
+resetButton.addEventListener('click',() =>{
+    let currentValue = 0;
+    console.log(`CURRENT VALUE = ${currentValue}`);
+})
