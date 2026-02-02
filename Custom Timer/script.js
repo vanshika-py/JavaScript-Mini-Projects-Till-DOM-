ff1455f485afdf44c56bf0f9a878082d65fdc6eb
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-02-03 03:16:14 +0530

    done selection of the elements in js with different classes

---
+++ Custom Timer/script.js
@@ -0,0 +1,5 @@
+let timer = document.querySelector('.timer');
+let hours = document.querySelector('.hours');
+let minutes  = document.querySelector('.minutes');
+let seconds = document.querySelector('seconds');
+let text = document.querySelector('.text');
