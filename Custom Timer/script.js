6e4c454465d05e773b1b2224ec132daaf794bfdf
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-02-04 01:02:50 +0530

    write some messy code for start button

--- Custom Timer/script.js
+++ Custom Timer/script.js
@@ -3,5 +3,19 @@ let start = document.querySelector('.start');
 let pause = document.querySelector('.pause');
 let reset = document.querySelector('.reset');
 let text = document.querySelector('.text');
+let hours = document.querySelector('.hours');
+let minutes = document.querySelector('.minutes');
+let seconds = document.querySelector('seconds');
+
+
+start.addEventListener('click', () => {
+    if(seconds = s-value){
+        let temp = setInterval(() => {
+            mm++;
+            mm.textContent = 1;
+    }
+        )}(),1000
+    });
+
 
 
