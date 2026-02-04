ca8ecd0293ccf7bdc88e4665e1d970b8fe8948c9
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-02-05 03:02:58 +0530

    change in messy code

--- Custom Timer/script.js
+++ Custom Timer/script.js
@@ -1,21 +1,26 @@
 let timer = document.querySelector('.timer');
-let start = document.querySelector('.start');
-let pause = document.querySelector('.pause');
-let reset = document.querySelector('.reset');
+let start = document.querySelector('.Start');
+let pause = document.querySelector('.Pause');
+let reset = document.querySelector('.Reset');
 let text = document.querySelector('.text');
 let hours = document.querySelector('.hours');
 let minutes = document.querySelector('.minutes');
-let seconds = document.querySelector('seconds');
+let seconds = document.querySelector('.seconds');
+
 
 
 start.addEventListener('click', () => {
-    if(seconds = s-value){
-        let temp = setInterval(() => {
-            mm++;
-            mm.textContent = 1;
-    }
-        )}(),1000
-    });
+    let temp = setInterval(() => {
+        secondsCount++; 
+     if( secondsCount === 60)
+      minutesCount++;
+      minutes.textContent = minutesCount;
+    if(minutesCount === 60)
+        hoursCount++;
+        secondsCount = 0;
+        hours.textContent = hoursCount;
+    console.log(temp)
+},1000)
+});
 
-
-
+ 
