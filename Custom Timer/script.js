6b04a81959cf83d642f6d4bfc5c4ebe81132c23f
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-02-08 22:09:29 +0530

    update in js start button

--- Custom Timer/script.js
+++ Custom Timer/script.js
@@ -7,9 +7,41 @@ let hours = document.querySelector('.hours');
 let minutes = document.querySelector('.minutes');
 let seconds = document.querySelector('.seconds');
 
+let secondsCount = 0;
+let minutesCount = 0;
+let hoursCount = 0;
 
 
 start.addEventListener('click', () => {
+    let temp = setInterval(() => {
+        secondsCount++;
+        seconds.textContent = secondsCount;
+        if(secondsCount === 60){
+            minutesCount++;
+            minutes.textContent = minutesCount;
+            secondsCount = 0;
+        }
+        if(minutesCount === 60){
+            hoursCount++;
+            hours.textContent = hoursCount;
+            minutesCount = 0;
+        }
+},1000)
+})
+
+
+reset.addEventListener('click', () =>{
+      clearInterval(temp)
+      minutesCount = 0;
+      minutes.textContent = minutesCount;
+      secondsCount = 0;
+      seconds.textContent = secondsCount;
+      hoursCount = 0;
+      hours.textContent = hoursCount;
+})
+
+
+/*start.addEventListener('click', () => {
     let temp = setInterval(() => {
         secondsCount++; 
      if( secondsCount === 60)
@@ -17,10 +49,13 @@ start.addEventListener('click', () => {
       minutes.textContent = minutesCount;
     if(minutesCount === 60)
         hoursCount++;
-        secondsCount = 0;
         hours.textContent = hoursCount;
     console.log(temp)
-},1000)
-});
+})
+});*/
+
+
+
 
  
+
