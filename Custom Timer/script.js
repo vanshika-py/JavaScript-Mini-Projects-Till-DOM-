8c6f60073a08779bc670c0bfa6c78ecf16bb5234
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-02-18 19:59:20 +0530

    made logics for this

--- Custom Timer/script.js
+++ Custom Timer/script.js
@@ -1,4 +1,64 @@
-let timer = document.querySelector('.timer');
+let start = document.querySelector('.Start');
+let pause = document.querySelector('.Pause');
+let reset = document.querySelector('.Reset');
+
+let hours = document.querySelector('.hours');
+let minutes = document.querySelector('.minutes');
+let seconds = document.querySelector('.seconds');
+
+let secondsCount = 0;
+let minutesCount = 0;
+let hoursCount = 0;
+
+let temp = null;
+
+
+start.addEventListener('click', () => {
+    if (temp === null) {
+        temp = setInterval(() => {
+            secondsCount++;
+            seconds.textContent = secondsCount;
+
+            if (secondsCount === 60) {
+                minutesCount++;
+                minutes.textContent = minutesCount;
+                secondsCount = 0;
+                seconds.textContent = 0;
+            }
+
+            if (minutesCount === 60) {
+                hoursCount++;
+                hours.textContent = hoursCount;
+                minutesCount = 0;
+                minutes.textContent = 0;
+            }
+        }, 1000);
+    }
+});
+
+
+pause.addEventListener('click', () => {
+    clearInterval(temp);
+    temp = null;
+});
+
+
+reset.addEventListener('click', () => {
+    clearInterval(temp);
+    temp = null;
+
+    secondsCount = 0;
+    minutesCount = 0;
+    hoursCount = 0;
+
+    seconds.textContent = 0;
+    minutes.textContent = 0;
+    hours.textContent = 0;
+});
+
+
+
+/*let timer = document.querySelector('.timer');
 let start = document.querySelector('.Start');
 let pause = document.querySelector('.Pause');
 let reset = document.querySelector('.Reset');
@@ -10,10 +70,13 @@ let seconds = document.querySelector('.seconds');
 let secondsCount = 0;
 let minutesCount = 0;
 let hoursCount = 0;
+let temp;
+
 
 
 start.addEventListener('click', () => {
-    let temp = setInterval(() => {
+    if(temp == null){
+     let temp = setInterval(() => {
         secondsCount++;
         seconds.textContent = secondsCount;
         if(secondsCount === 60){
@@ -27,10 +90,25 @@ start.addEventListener('click', () => {
             minutesCount = 0;
         }
 },1000)
+    }
+})
+
+if (secondsCount === 60) {
+                minutesCount++;
+                minutes.textContent = minutesCount;
+                secondsCount = 0;
+                seconds.textContent = 0;
+
+
+pause.addEventListener('click',() =>{
+
+
 })
 
 
+
 reset.addEventListener('click', () =>{
+      if(temp !== null)
       clearInterval(temp)
       minutesCount = 0;
       minutes.textContent = minutesCount;
@@ -39,6 +117,9 @@ reset.addEventListener('click', () =>{
       hoursCount = 0;
       hours.textContent = hoursCount;
 })
+
+
+
 
 
 /*start.addEventListener('click', () => {
@@ -54,8 +135,4 @@ reset.addEventListener('click', () =>{
 })
 });*/
 
-
-
-
- 
 
