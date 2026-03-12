21d1cc8b5f93c94e2a10aa6cce9cb0bfbe88ba84
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-03-12 23:31:40 +0530

    selected elements

---
+++ Weather App/script.js
@@ -0,0 +1,12 @@
+let searchBtn = document.querySelector('.searchBtn');
+let submitBtn = document.querySelector('.submit');
+let city = document.querySelector('.city-name');
+
+
+let temperature = document.querySelector('.Weather-icon');
+let description = document.querySelector('.description')
+
+submitBtn.addEventListener('click',()=>{
+    fetch
+})
+
