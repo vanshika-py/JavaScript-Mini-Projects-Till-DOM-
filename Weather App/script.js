419fcc6c36f4b673fb1ea9d3887eede19b024f98
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-03-14 20:03:36 +0530

    added button logic

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,12 +1,28 @@
-let searchBtn = document.querySelector('.searchBtn');
+let city = document.querySelector('.inputCity');
 let submitBtn = document.querySelector('.submit');
-let city = document.querySelector('.city-name');
+let cityName = document.querySelector('.city-name');
+let temperature = document.querySelector('.temperature');
+let Weather= document.querySelector('.Weather-icon');
+let details = document.querySelector('.description');
 
 
-let temperature = document.querySelector('.Weather-icon');
-let description = document.querySelector('.description')
+
+
+const api_key = "ea8b021707ea9cab1d6c61024be7a521";
 
 submitBtn.addEventListener('click',()=>{
-    fetch
+    const userInput = city.value.trim();
+    if(userInput != ""){
+        const url  = fetch(`http://api.weatherstack.com/current?access_key=ea8b021707ea9cab1d6c61024be7a521&query= ${userInput}`)
+    .then(response => response.json())
+    .then(data =>{
+      temperature.textContent = data.current.temperature;
+      cityName.textContent = data.current.cityName;
+      Weather.textContent = data.current.Weather-isSecureContext;
+      
+    })
+    
+
+
 })
 
