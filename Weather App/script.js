fc164ac1f8f1e701bb17a8dfdb95e7f0341fcc43
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-04-05 00:49:01 +0530

    converted raw data to json format

--- Weather App/script.js
+++ Weather App/script.js
@@ -7,7 +7,11 @@ const api_key = "aac679675fda466dab4131950260404";
 submitBtn.addEventListener('click',()=>{
      const city = userInput.value.trim()
      if(city != ""){
-     const api = fetch(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=mumbai`)
+     const api = fetch(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${city}`)
+     .then(Response => Response.json())
+     .then (data => {
+        console.log(data)
+     })
     })
 
-  const api = fetch(`http://api.weatherapi.com/v1/current.json?key=aac679675fda466dab4131950260404&q=delhi`)
+  
