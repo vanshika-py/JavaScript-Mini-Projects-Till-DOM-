ac5869ac5da1ed6130db1c11dd76cf1c1dc31540
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-04-18 23:07:43 +0530

    added a new div to the htmnl

---
+++ Calculator Program/script.js
@@ -0,0 +1,6 @@
+let remove = document.querySelector('.AC');
+let percentage = document.querySelector('.percent');
+let backRemove = document.querySelector(',back');
+let divide = document.querySelector('.division');
+
+
